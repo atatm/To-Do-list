3d301162a1ff2905cfc2c5684a082ec462a208ff
atatm <alelignayana@gmail.com>
2022-11-11 23:30:49 +0300

    Make the task to be edited

---
+++ src/getElement.js
@@ -0,0 +1,17 @@
+export default function getPosition(container, y) {
+  const draggElement = [
+    ...container.querySelectorAll('.draggables:not(.dragging'),
+  ];
+
+  return draggElement.reduce(
+    (closest, child) => {
+      const box = child.getBoundingClientRect();
+      const offset = y - box.top - box.height / 2;
+      if (offset < 0 && offset > closest.offset) {
+        return { offset, element: child };
+      }
+      return closest;
+    },
+    { offset: Number.NEGATIVE_INFINITY },
+  ).element;
+}
